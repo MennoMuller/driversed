1337789089292e6186a9eb6d0de144e7db583605
MennoMuller <menno.muller.nl@gmail.com>
2023-02-17 09:41:35 +0100

    Initial commit

---
+++ src/main/java/com/driversed/driversed/DriversedApplication.java
@@ -0,0 +1,13 @@
+package com.driversed.driversed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriversedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriversedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/driversed/driversed/DriversedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driversed.driversed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriversedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
