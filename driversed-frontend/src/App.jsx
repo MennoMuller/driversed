6593f075c1bc9c7c8586edaf4cad42ae5e69ed4c
MennoMuller <menno.muller.nl@gmail.com>
2023-03-03 13:25:06 +0100

    Made account the starting page

--- driversed-frontend/src/App.jsx
+++ driversed-frontend/src/App.jsx
@@ -125,7 +125,7 @@ function App() {
             }
           />
           <Route
-            path="/account"
+            path="/"
             element={
               <AccountPage user={user} setUser={setUser} />
             }
--- driversed-frontend/src/components/Header.jsx
+++ driversed-frontend/src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function Header(props) {
+  const name = props.user.username;
   return (
     <div className="w-screen bg-slate-600 p-3">
       <h1 className="mb-2 text-5xl font-bold">
@@ -9,7 +10,7 @@ function Header(props) {
       </h1>
       <nav>
         <NavLink
-          to="/account"
+          to="/"
           className="rounded p-2 text-lg text-black hover:opacity-80 dark:text-white"
         >
           Account
@@ -17,18 +18,36 @@ function Header(props) {
         <NavLink
           to="/people"
           className="rounded p-2 text-lg text-black hover:opacity-80 dark:text-white"
+          hidden={
+            !(
+              name &&
+              props.user.roles.includes("ROLE_ADMIN")
+            )
+          }
         >
           People
         </NavLink>
         <NavLink
           to="/instructor"
           className="rounded p-2 text-lg text-black hover:opacity-80 dark:text-white"
+          hidden={
+            !(
+              name &&
+              props.user.roles.includes("ROLE_INSTRUCTOR")
+            )
+          }
         >
           Instructor
         </NavLink>
         <NavLink
           to="/student"
           className="rounded p-2 text-lg text-black hover:opacity-80 dark:text-white"
+          hidden={
+            !(
+              name &&
+              props.user.roles.includes("ROLE_STUDENT")
+            )
+          }
         >
           Student
         </NavLink>
